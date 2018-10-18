e1f2bec9fb415caa287a1d9835a8ba20beacbaa4
gfot <georgios.fotiadis@gmail.com>
2018-10-18 13:50:25 +0300

    New

--- scrapper.py
+++ scrapper.py
@@ -2,24 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 from fake_useragent import UserAgent
 
+
 def read_file(filename):
-    with open(filename,'r') as f:
+    with open(filename, 'r') as f:
         data = f.read()
     return data
 
+
+# Get HTML site and store it at 'data'
 # filename='D:\_gfot\PyCharmProjects\web_scraping\intro-to-soup-html.html'
-#
-# my_data = read_file(filename)
-# soup = BeautifulSoup(my_data,'lxml')
-# print(soup)
+# data = read_file(filename)
+
+# url = 'http://www.euroleague.net/competition/players/showplayer?pcode=JUO&seasoncode=E2018'
+# url = 'https://gr.linkedin.com/in/georgios-fotiadis-5a48035'
+url = 'http://www.google.com'
+ua = UserAgent()
+header = {'user-agent':ua.chrome}
+response = requests.get(url, headers=header)
+data = response.content
 
 
-url = 'http://www.euroleague.net/competition/players/showplayer?pcode=JUO&seasoncode=E2018'
-response = requests.get(url)
-data = response.text
+# Parse HTML site
 soup = BeautifulSoup(data, 'lxml')
-# soup = BeautifulSoup(data, 'html.parser')
-print(soup)
+# print(soup.prettify())
+meta = soup.meta
+print(meta)
+
+
 # tags = soup.find_all('a')
 # print(tags)
 # for tag in tags:
