f9e64e96ccc31160025a530a2e2b0387a61c377c
gfot <georgios.fotiadis@gmail.com>
2018-11-07 16:00:40 +0200

    New

--- instagram_scrapper.py
+++ instagram_scrapper.py
@@ -1,98 +1,60 @@
-from bs4 import BeautifulSoup
-import requests
-from fake_useragent import UserAgent
 from selenium import webdriver
 from time import sleep
 
 
-def get_url_data(url,ua,header):
-    '''
-    Returns Beautiful Soup of the requested URL
-    '''
-    response = requests.get(url, headers=header)
-    data = response.content
-    return BeautifulSoup(data, 'lxml')
+class Insta_App:
+
+    def __init__(self, username, password, path):
+        self.username = username
+        self.password = password
+        self.path = path
+        self.driver = webdriver.Chrome('D:\_gfot\chromedriver')
+        self.main_url = 'https://www.instagram.com'
+        # self.main_url = 'https://www.instagram.com/accounts/login/?source=auth_switcher'
+        self.driver.get(self.main_url)
+
+    def login(self):
+        # Show login screen
+        login_button = self.driver.find_element_by_link_text('Log in')
+        # login_button = self.driver.find_element_by_xpath("//div[@class='gr27e']/p[@class='izU2O']/a")
+        login_button.click()
+        sleep(2)
+
+        # Login to instagram account
+        username_textbox = self.driver.find_element_by_xpath("//div[@class='f0n8F ']/input[@name='username']")
+        username_textbox.send_keys(self.username)
+        password_textbox = self.driver.find_element_by_xpath("//div[@class='f0n8F ']/input[@name='password']")
+        password_textbox.send_keys(self.password)
+        password_textbox.submit()
+        sleep(3)
+
+        # Select 'Not Now' for notification popup (if there)
+        try:
+            self.driver.find_element_by_xpath("//button[text()='Not Now']").click()
+        except:
+            pass
 
 
-def read_file(filename):
-    with open(filename, 'r') as f:
-        data = f.read()
-    return data
+    def select_target_profile(self, target_username):
+        target_url = self.main_url + '/' + target_username
+        self.driver.get(target_url)
 
-
-########################################################################################
-## Get HTML site and store it at 'data'
-
-# filename=''
-# data = read_file(filename)
-
-base_url = 'https://codingbat.com'
-ua = UserAgent()
-header = {'user-agent':ua.chrome}
-soup = get_url_data(base_url+'/python',ua,header)
-# print('-------------------------\n',soup.prettify(),'\n-------------------------')
-
-# fd = open('instagram_data.txt','w',encoding="utf-8")
-# fd.write('Scrapping https://www.instagram.com\n\n')
-
-driver = webdriver.Chrome('D:\_gfot\chromedriver')
-driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-soup = BeautifulSoup(driver.page_source,'lxml')
-# print(soup.prettify())
-
-signup_button = driver.find_element_by_xpath("//span[@id='react-root']//p[@class='izU2O']/a")
-signup_button.click()
-
-
-## Login to Instagram account
-# username_textbox = driver.find_element_by_name('username')
-# username_textbox.send_keys('fotgio@hotmail.com')
-# password_textbox = driver.find_element_by_name('password')
-# password_textbox.send_keys('xxxxx')
-# username_textbox.submit()
-sleep(5)
-
-# # Get names and category URLs (urls = category URLs)
-# all_divs = soup.find_all('div',attrs={'class':'summ'})
-# urls = {}
-# for div in all_divs:
-#     urls[div.a.string] = base_url+div.a['href']
-# print(urls)
+# driver = webdriver.Chrome('D:\_gfot\chromedriver')
+# driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+# soup = BeautifulSoup(driver.page_source,'lxml')
+# # print(soup.prettify())
 #
-# # Get names and question URLs (urls_2 = question URLs)
-# for key,url in urls.items():
-#     print('\n'+url)
-#     # fd.write('\n\n\n--------------------------------------------\n')
-#     # fd.write('-> '+key)
-#     inner_soup = get_url_data(url, ua, header)
-#     div = inner_soup.find('div',attrs={'class':'tabc'})
-#     urls_2 = {}
-#     for td in div.table.find_all('td'):
-#         urls_2[td.a.string] = base_url+td.a['href']
-#     print(urls_2)
-#
-#     # Get question info
-#     for key_2,url_2 in urls_2.items():
-#         try:
-#             print('\n' + url_2)
-#             # fd.write('\n\n---> ' + key_2)
-#             inner_soup_2 = get_url_data(url_2, ua, header)
-#             div_2 = inner_soup_2.find('div',attrs={'class':'indent'})
-#             problem_statement = div_2.table.div.string
-#             print(problem_statement)
-#             # fd.write('\n' + problem_statement)
-#
-#             statement_siblings = div_2.table.div.next_siblings
-#             examples = [sibling.string for sibling in statement_siblings if sibling.string is not None]
-#             print(examples)
-#             for my_example in examples:
-#                 # fd.write('\n'+my_example.string)
-#                 print(my_example.string)
-#             break
-#         except:
-#             pass
-#
-#     break
+# signup_button = driver.find_element_by_xpath("//span[@id='react-root']//p[@class='izU2O']/a")
+# signup_button.click()
 
-# fd.close()
-driver.close()
+
+if __name__ == '__main__':
+    insta_app = Insta_App('fotgio@hotmail.com', 'xxxxx', 'D:\_gfot\PyCharmProjects\web_scraping\insta_data')
+    insta_app.login()
+    insta_app.select_target_profile('_gfot')
+
+    input('Press any key to continue')
+    insta_app.driver.close()
+
+
+
